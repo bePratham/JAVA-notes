a7fbc1dcba5696e8c1ada9775c3648df66cfe533
Pratham Singh <prathamsinghkathayat@gmail.com>
2024-04-27 23:13:20 +0530

    testing change

--- Vishva/src/main/java/com/example/Vishva/VishvaApplication.java
+++ Vishva/src/main/java/com/example/Vishva/VishvaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VishvaApplication {
-
+ //rest
 	public static void main(String[] args) {
 		SpringApplication.run(VishvaApplication.class, args);
 	}
